53e56a19b9bef13339dbb62fded8d0ed5dd78b05
Bkeane201 <bkeane117@gmail.com>
2021-04-20 07:40:23 -0700

    Experimentation with GitCommits

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,10 +7,55 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SecondView: View {
+    var name: String
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Dismiss") {
+            self.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+struct User: Codable {
+    var firstName: String
+    var lastName: String
+}
+
+struct ContentView: View {
+    @State private var showingSheet  = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    
+    var body: some View {
+        NavigationView {
+                VStack {
+                    Button("Tap count: \(tapCount)") {
+                        self.tapCount += 1
+                        UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+                    }
+                    List {
+                        ForEach(numbers, id: \.self) {
+                            Text("\($0)")
+                        }
+                        .onDelete(perform: removeRows)
+                    }
+                    Button("Add Number") {
+                        self.numbers.append(self.currentNumber)
+                        self.currentNumber += 1
+                    }
+                    Button("Show Sheet") {
+                        self.showingSheet.toggle()
+                    }
+                    .sheet(isPresented: $showingSheet) {
+                        SecondView(name: "@bkeane")
+                    }
+                }
+                .navigationBarItems(leading: EditButton())
+            }
+        }
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
